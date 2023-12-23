d13a7cbc2d48e3c70870d70f2306a4f8102491cc
Sergey Missonov <missonov@mail.ru>
2023-12-23 13:35:24 +0400

    motors_control.py

--- motors_control.py
+++ motors_control.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import time
+import RPi.GPIO as GPIO
+
+# Подготавливаем пины GPIO.
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.OUT)
+GPIO.output(4, GPIO.LOW)
+
+GPIO.setup(17, GPIO.OUT)
+GPIO.output(17, GPIO.LOW)
+
+# Включаем вращение двигателя 1 в одну сторону.
+GPIO.output(4, GPIO.HIGH)
+
+# ждем 5 секунд.
+time.sleep(5)
+
+# Выключаем двигатель 1.
+GPIO.output(4, GPIO.LOW)
